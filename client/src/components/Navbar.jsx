eea32095dc7936c2fac3550108f15ee483386885
OK-Emmanuel <olawuniemmanuelk@gmail.com>
2025-04-12 17:57:41 +0100

    fixed logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
