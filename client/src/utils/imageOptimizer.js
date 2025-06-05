6f0ad576c974516e629d74d62bd69371472530b8
Olawuni Emmanuel Kayode <121316825+OK-Emmanuel@users.noreply.github.com>
2025-06-05 19:13:05 +0100

    updated imageoptimizer

--- client/src/utils/imageOptimizer.js
+++ client/src/utils/imageOptimizer.js
@@ -1,15 +1,16 @@
 // utils/imageOptimizer.js
 
-export const optimizeImageUrl = (url, width = 500, height = 300, format = 'webp', quality = 'auto') => {
+export const optimizeImageUrl = (url, width, height, format = 'webp', quality = 'auto') => {
     // If the URL is not from Cloudinary, return the original URL
     if (!url.includes('cloudinary.com')) {
       return url;
     }
   
-    // Build the optimized URL by adding Cloudinary transformations
-    const transformedUrl = url
-      .replace(/\/upload\//, `/upload/f_${format},q_${quality},w_${width},h_${height},c_fill/`);
+    // Build transformation string dynamically
+    let transformation = `f_${format},q_${quality}`;
+    if (width) transformation += `,w_${width}`;
+    if (height) transformation += `,h_${height},c_fill`;
     
-    return transformedUrl;
+    return url.replace(/\/upload\//, `/upload/${transformation}/`);
   };
   
